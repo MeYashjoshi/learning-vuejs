670a5e81f993ea724c16b10995ee168052ed5ad6
Yash Joshi <meyashjoshi@outlook.in>
2026-03-03 11:44:47 +0530

    feat: add assignments component with json-server mock backend (Episode 14)

---
+++ Episode14/components/AssignmentCreate.js
@@ -0,0 +1,26 @@
+export default {
+  template: `
+    
+        <form @submit.prevent="add">
+            <div class="border border-gray-600 flex">
+              <input v-model="newAssignment" placeholder="New Assignments..." class="text-black p-2"/>
+              <button type="submit" class="text-black p-2 border-l border-gray-600">Add</button>
+            </div>
+        </form>
+
+    `,
+
+  data() {
+    return {
+      newAssignment: "",
+    };
+  },
+
+  methods: {
+    add() {
+      this.$emit("add", this.newAssignment);
+
+      this.newAssignment = "";
+    },
+  },
+};
