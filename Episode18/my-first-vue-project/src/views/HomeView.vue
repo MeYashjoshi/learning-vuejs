1f70d3a653cdcb0841918a01707d9c359fa7cc5b
Yash Joshi <meyashjoshi@outlook.in>
2026-03-09 11:14:24 +0530

    feat: initialize Episode18 Vue project with Vite, add basic structure and components
    - Created package.json with dependencies for Vue, Vue Router, and development tools.
    - Added favicon.ico for the project.
    - Implemented main App.vue component with routing links.
    - Created base CSS for styling and layout.
    - Added logo.svg for branding.
    - Developed main.css for application styling.
    - Created HelloWorld component to display a greeting message.
    - Implemented TheWelcome component with various welcome items and icons.
    - Added WelcomeItem component for displaying individual welcome items.
    - Created icon components for community, documentation, ecosystem, support, and tooling.
    - Set up main.js to initialize Vue application and router.
    - Configured router with home, about, and contact views.
    - Developed AboutView and ContactView components with basic content.
    - Created HomeView component with dynamic message and input.
    - Configured Vite for the project with necessary plugins.

---
+++ Episode18/my-first-vue-project/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue';
+  import { ref } from 'vue';
+
+    let message= ref("Hello, World!");
+
+    setTimeout(()=>{
+
+      message.value = "I have been changed";
+
+    },2000);
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <p>{{message }}</p>
+
+    <input type="text" v-model="message">
+  </main>
+</template>
