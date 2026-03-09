971013384ca686c2e43e62fce7f6b6bd68977a73
Yash Joshi <meyashjoshi@outlook.in>
2026-03-09 11:16:43 +0530

    feat: initialize Episode19 Vue project with Vite, add basic structure and components
    - Created package.json with dependencies for Vue, Vue Router, and development tools.
    - Implemented main App.vue with navigation links and HelloWorld component.
    - Developed main.css for layout and design.
    - Added HelloWorld component to display a greeting message.
    - Created TheWelcome component with various WelcomeItem components for documentation and community support.
    - Implemented WelcomeItem component for reusable welcome items.
    - Added icon components for community, documentation, ecosystem, tooling, and support.
    - Set up main.js to initialize Vue app and router.
    - Configured router with routes for home, about, and contact views.
    - Developed AboutView and ContactView components with basic structure.
    - Created HomeView component with dynamic message functionality.
    - Configured Vite for Vue with necessary plugins and aliasing.

---
+++ Episode19/my-first-vue-project/src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<script>
+
+import flash from '../../mixins/flash';
+
+export default{
+
+mixins:[flash]
+
+}
+
+
+</script>
+
+
+
+
+<template>
+  <div class="about">
+      <button @click="falsh('It works on the about page!')">Click Me</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
